61f6ffd6e803f8d9b3c8fd17b4b239da52034eab
shadyman <m.m.shdmn@gmail.com>
2023-04-04 09:37:44 +0600

    getCoef method

--- Main9.java
+++ Main9.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 
 public class Main9 {
-    public static int coef(int n, int k) {
+    /*
+        @getCoef - find binomial coefficient of two numbers
+        @param1 - integer number (n)
+        @param2 - integer number (k)
+        @return - 1 integer number(binomial coefficient)
+    */
+    public static int getCoef(int n, int k) {
         if (k == 0 || k == n) {
             return 1;
         } else {
-            return coef(n-1, k-1) + coef(n-1, k);
+            return getCoef(n-1, k-1) + getCoef(n-1, k);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(coef(scanner.nextInt(),scanner.nextInt()));
+        System.out.println(getCoef(scanner.nextInt(),scanner.nextInt()));
     }
 }
