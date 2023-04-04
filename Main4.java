4a5f1692d93c5465fd3ed6857a558fd200421d02
shadyman <m.m.shdmn@gmail.com>
2023-04-04 09:23:31 +0600

    findFactorial method

--- Main4.java
+++ Main4.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class Main4 {
-    public static int fact(int n) {
+    /*
+        @findFactorial - find factorial of number
+        @param - integer number (n)
+        @return - integer number
+    */
+    public static int findFactorial(int n) {
         if (n == 0 || n == 1) {
             return 1;
         } else {
-            return n * fact(n-1);
+            return n * findFactorial(n-1);
         }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        System.out.println(fact(N));
+        System.out.println(findFactorial(N));
     }
 }
