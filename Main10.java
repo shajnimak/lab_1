0bb283961ae8eaeeedafdece840257fe4796bfa0
shadyman <m.m.shdmn@gmail.com>
2023-04-04 09:38:57 +0600

    getGcd method

--- Main10.java
+++ Main10.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class Main10 {
-    public static int gcd(int a, int b) {
+    /*
+        @getGcd - find gcd of 2 numbers
+        @param1 - integer number (a)
+        @param2 - integer number (b)
+        @return - gcd (integer)
+    */
+    public static int getGcd(int a, int b) {
         if (b == 0) {
             return a;
         } else {
-            return gcd(b, a % b);
+            return getGcd(b, a % b);
         }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(gcd(scanner.nextInt(), scanner.nextInt()));
+        System.out.println(getGcd(scanner.nextInt(), scanner.nextInt()));
     }
 }
