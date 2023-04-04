e53c02a1fe286d064d15014db5cfa81f502bd649
shadyman <m.m.shdmn@gmail.com>
2023-04-04 09:32:19 +0600

    getReverse method

--- Main7.java
+++ Main7.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 
 public class Main7 {
-    public static void reverse(int[] arr, int start, int end) {
+    /*
+        @getReverse - get reversed list
+        @param1 - integer array (array)
+        @param2 - integer number (start) reverse start index
+        @param3 - integer number (end) reverse end index
+        @return - reversed list
+    */
+    public static void getReverse(int[] arr, int start, int end) {
         if (start >= end) {
             return;
         }
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-        reverse(arr, start+1, end-1);
+        getReverse(arr, start+1, end-1);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +24,7 @@ public class Main7 {
         for (int i = 0; i < N; i++){
             array[i] = scanner.nextInt();
         }
-        reverse(array, 0, N-1);
+        getReverse(array, 0, N-1);
         for (int i : array){
             System.out.print(i + " ");
         }
